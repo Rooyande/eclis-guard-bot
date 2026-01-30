5f3f026af1f000f4ddc1eb9f5ea01a15a311f3a1
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-30 17:21:59 +0330

    Update states.py

--- app/states.py
+++ app/states.py
@@ -3,8 +3,14 @@ from aiogram.fsm.state import StatesGroup, State
 
 class OwnerStates(StatesGroup):
     waiting_for_admin_id = State()
+    waiting_for_clone_target_ids = State()
+    waiting_for_set_context_chat_id = State()
 
 
 class AdminStates(StatesGroup):
     waiting_for_safe_user_id = State()
-    waiting_for_unban_user_id = State()
+    waiting_for_remove_safe_user_id = State()
+    waiting_for_set_context_chat_id = State()
+    waiting_for_create_folder_name = State()
+    waiting_for_folder_add_user_id = State()
+    waiting_for_folder_remove_user_id = State()
